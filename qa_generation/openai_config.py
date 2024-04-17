301a3123590392fe69891e581ee39a202cf5256d
Jing Zhang <ankie.zj@gmail.com>
2024-04-17 11:34:44 -0400

    Add instruction in openai_config

---
+++ qa_generation/openai_config.py
@@ -0,0 +1,9 @@
+
+"""
+Note:
+Use `git update-index --assume-unchanged qa_generation/openai_config.py` to ignore changes to this file.
+"""
+
+class OpenAIConfig:
+    PROXY_BASE_URL = "https://cmu.litellm.ai"
+    PROXY_API_KEY = "todo"
