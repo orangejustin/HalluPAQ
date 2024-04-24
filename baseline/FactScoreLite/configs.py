1e4296543f3a9141e063571a6156c4b0c9af6106
Jing Zhang <ankie.zj@gmail.com>
2024-04-24 00:01:45 -0400

    Add the FactScore as the baseline

---
+++ baseline/FactScoreLite/configs.py
@@ -0,0 +1,19 @@
+import importlib.resources
+from pathlib import Path
+# Path to the data file within the package
+data_path = importlib.resources.files("FactScoreLite") / "data"
+
+# Path to the data file within the package
+atomic_facts_demons_path = data_path / "atomic_facts_demons.json"
+fact_scorer_demons_path = data_path / "fact_scorer_demons.json"
+
+# OpenAI API
+max_tokens = 1024
+temp = 0.7
+model_name = "gpt-4-turbo-preview"
+
+# Database path
+# Current folder
+current_folder = Path(__file__).parent
+facts_db_path = current_folder / "database/facts.json"
+decisions_db_path = current_folder / "database/decisions.json"
