dbeef2e137849201ccf155ad10e9f4d91fdad9ac
Jing Zhang <ankie.zj@gmail.com>
2024-04-25 12:17:37 -0400

    Modify the FactScoreEvaluator based on the change in FactScoreLite

--- baseline/factscore.py
+++ baseline/factscore.py
@@ -9,18 +9,19 @@ class FactScoreEvaluator:
     FactScore original repository: https://github.com/shmsw25/FActScore/tree/main
     FactScoreLite repository: https://github.com/armingh2000/FactScoreLite/tree/main?tab=readme-ov-file
     """
-    def __init__(self, gamma:int = 10):
+    def __init__(self, gamma:int = 10, db_id:str = ""):
         os.environ['OPENAI_API_KEY'] = OpenAIConfig.OpenAI_API_KEY
-        self.fact_score = FactScore(gamma)
+        self.fact_score = FactScore(gamma, db_id)
 
     def get_batch_fact_score(self, generations:list[str], knowledge_sources:list[str]) -> tuple[list[float], list[float]]:
         scores, init_scores = self.fact_score.get_factscore(generations, knowledge_sources)
+        self.fact_score.delete_db()
 
         return scores, init_scores
     
     def get_single_fact_score(self, generation:str, knowledge_source:str) -> tuple[float, float]:
         score, init_score = self.fact_score.get_factscore([generation], [knowledge_source])
-
+        self.fact_score.delete_db()
         return score, init_score
 
 if __name__ == '__main__':
@@ -29,11 +30,12 @@ if __name__ == '__main__':
     knowledge_sources = "Morton Neuroma -- Enhancing Healthcare Team Outcomes. Morton neuroma is best managed non-surgically with an interprofessional team of healthcare professionals, including a podiatrist, orthopedic surgeon, sports physician, nurse practitioner, and primary care provider. The patient may require pain medications, but the key is changes in shoe wear. The nurse should encourage the patient to wear appropriate, well-padded, non-constrictive shoewear. Obese patients may benefit from weight loss, so a dietary consult is appropriate. The patient may benefit from physical therapy and the use of warm compresses and ice to ease the pain."
     
     # Get the fact score for the covered data
-    factscore = FactScoreEvaluator()
+    factscore = FactScoreEvaluator(db_id="demo1")
     score1, init_score1 = factscore.get_single_fact_score(generation1, knowledge_sources)
+    
 
     # Get the fact score for the uncovered data
-    factscore = FactScoreEvaluator()
+    factscore = FactScoreEvaluator(db_id="demo2")
     score2, init_score2 = factscore.get_single_fact_score(generation2, knowledge_sources)
 
     print("Fact score for covered data:", score1, init_score1)
